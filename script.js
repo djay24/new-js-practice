f5fea3b930b37edb26ff28a674be3fa857601fd7
DJ Rodriguez <djayr24@gmail.com>
2018-11-06 19:04:38 -0800

    finished coding challenge 1

--- script.js
+++ script.js
@@ -70,27 +70,43 @@
  * operator precendence 
  */
 
- var now = 2018; 
- var yearJohn = 1989;
- var fullAge = 18; 
+//  var now = 2018; 
+//  var yearJohn = 1989;
+//  var fullAge = 18; 
 
- // multiple operators
- var isFullAge = now - yearJohn >= fullAge; // true
-console.log(isFullAge);
+//  // multiple operators
+//  var isFullAge = now - yearJohn >= fullAge; // true
+// console.log(isFullAge);
 
-// grouping 
-var ageJohn = now - yearJohn;
-var ageMark = 35;
-var average = (ageJohn + ageMark) / 2;
-console.log(average);
+// // grouping 
+// var ageJohn = now - yearJohn;
+// var ageMark = 35;
+// var average = (ageJohn + ageMark) / 2;
+// console.log(average);
 
-// multiple assignments
-var x, y;
-x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
-console.log(x, y);
+// // multiple assignments
+// var x, y;
+// x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+// console.log(x, y);
 
-// More operators 
-x *= 2;  // 26 * 2
-console.log(x);
-x += 10; // 26 * 2 + 10
-console.log(x);
+// // More operators 
+// x *= 2;  // 26 * 2
+// console.log(x);
+// x += 10; // 26 * 2 + 10
+// console.log(x); 
+
+/************
+ * coding challenge
+ */
+
+ var johnMass = 92; // kg
+ var markMass = 78;
+
+ var johnHeight = 1.95; // meters
+ var markHeight = 1.69;
+
+ var johnBMI = johnMass / (johnHeight * johnHeight);
+ var markBMI = markMass / (markHeight * markHeight);
+
+ var bmiCompare = johnBMI > markBMI;
+ console.log(johnBMI.toFixed(1), markBMI.toFixed(1), bmiCompare);
