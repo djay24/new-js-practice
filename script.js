7dbf5d8a69b44dd0a2f1bfb762931b886e3cca0a
DJ Rodriguez <djayr24@gmail.com>
2018-11-06 17:37:06 -0800

    basic operators

--- script.js
+++ script.js
@@ -21,22 +21,51 @@
  * variable mutation and type coercion
  */
 
-var firstName = 'DJ';
-var age = 21;
-console.log(firstName + ' ' + age);
-//type coercion
-var job, isMarried;
+// var firstName = 'DJ';
+// var age = 21;
+// console.log(firstName + ' ' + age);
+// //type coercion
+// var job, isMarried;
 
-job = 'produce worker';
-isMarried = false;
+// job = 'produce worker';
+// isMarried = false;
 
-console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he Married? ' + isMarried);
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he Married? ' + isMarried);
 
-// variable mutation
-age = 'twenty one';
-job = 'boxer';
+// // variable mutation
+// age = 'twenty one';
+// job = 'boxer';
 
 //  alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he Married? ' + isMarried);
 
 // var lastName = prompt('What is his last name?');
 // console.log(lastName);
+
+/************
+ * Basic operators
+ */
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33
+
+// math operators
+yearJohn =  now - ageJohn;
+yearMark = now - ageMark;
+console.log(yearJohn, yearMark);
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'mark is older');
+
+
+
+
