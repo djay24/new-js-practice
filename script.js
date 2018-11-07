02de5232aa79e99e8d6bc11f578abc2cbea6c050
DJ Rodriguez <djayr24@gmail.com>
2018-11-06 17:17:15 -0800

    did data type and mutation lessions

---
+++ script.js
@@ -0,0 +1,42 @@
+/***********
+ * variables and data types
+ */
+
+// var firstName = 'DJ';
+
+// var lastName = 'Rodriguez'; // in '' is a string
+// var age = 21; // is a number
+// console.log(firstName, lastName);
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job; 
+// console.log(job);
+
+// job = 'Teacher';
+// console.log(job);
+
+/***********
+ * variable mutation and type coercion
+ */
+
+var firstName = 'DJ';
+var age = 21;
+console.log(firstName + ' ' + age);
+//type coercion
+var job, isMarried;
+
+job = 'produce worker';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he Married? ' + isMarried);
+
+// variable mutation
+age = 'twenty one';
+job = 'boxer';
+
+//  alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he Married? ' + isMarried);
+
+// var lastName = prompt('What is his last name?');
+// console.log(lastName);
