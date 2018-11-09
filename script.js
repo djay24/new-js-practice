487f5c3cc611038a8adff981cd1254820cbc21d4
DJ Rodriguez <djayr24@gmail.com>
2018-11-08 16:07:05 -0800

    terinary and swich statements

--- script.js
+++ script.js
@@ -153,15 +153,73 @@
  * Boolean logic
  */
 
- var firstName = 'John';
- var age = 20;
+//  var firstName = 'John';
+//  var age = 20;
 
- if (age < 13 ) {
-     console.log(firstName + ' is a boy')
- } else if (age >= 13 && age < 20 ) {
-    console.log(firstName + ' is a teenager')
-    } else if (age >= 20 && age < 30) {
-        console.log(firstName + ' is a young man')
-    } else {
-    console.log(firstName + ' is a man')
- }
+//  if (age < 13 ) {
+//      console.log(firstName + ' is a boy')
+//  } else if (age >= 13 && age < 20 ) {
+//     console.log(firstName + ' is a teenager')
+//     } else if (age >= 20 && age < 30) {
+//         console.log(firstName + ' is a young man')
+//     } else {
+//     console.log(firstName + ' is a man')
+//  };
+
+/*******
+ * Ternary opererator and switch statements
+ */
+
+// var firstName = 'John';
+// var age = 21;
+
+// // Ternary 
+// age >= 18 ? console.log(firstName + ' can drink a beer.')
+// : console.log(firstName + ' drinks juice.');
+
+// var drink = age >= 18 ? 'Beer' : 'Juice';
+// console.log(drink)
+
+// this is how you would do this with a if else
+// if (age >= 18) {  
+//     var drink = 'beer';
+// } else {
+//     var drink = 'juice';
+// }
+
+// Switch statement
+// var job = 'teacher';
+// switch (job) {
+//     case 'teacher':
+//     case 'instructor':
+//         console.log(firstName + ' teaches kids how to code');
+//         break;
+//     case 'driver':
+//         console.log(firstName + ' drives for uber');
+//         break;
+//     case 'designer': 
+//         console.log(firstName + ' designs beautiful websites.');
+//         break;
+//     default: 
+//     console.log(firstName + ' does something else');
+// }
+
+
+// switch (true) {
+//     case age < 13: 
+//         console.log(firstName + ' is a boy');
+//         break;
+//     case age >= 13 && age < 20:
+//         console.log(firstName + ' is a teenager');
+//         break;
+//     case age >= 20 && age < 30: 
+//         console.log(firstName + ' is a young man');
+//         break;
+//     default: 
+//         console.log(firstName + ' is a man');
+// }
+
+/*********
+ *  Truthy and Falsy Values and equality operators
+ */
+
